63d8c3194c1197e563cfc0df57f873f9da3c20c0
alexonozor <alex.onozor@charisol.io>
2019-07-04 16:52:26 +0100

    deploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const user = require('./src/input');
+const path = require('path');
 
 
 // console.log that your server is up and running
@@ -10,4 +11,11 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // create a GET route
 app.get('/user/:id', (req, res) => {
   res.send(user);
+});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./build'));
+
+app.get('/*', function(req,res) { 
+   res.sendFile(path.join(__dirname,'/build/index.html'));
 });
